2c8c0559ca2a16cbef324ceb42fd2f78d6618ba6
nopsky <cnnopsky@gmail.com>
2021-10-25 13:35:33 +0800

    feat:新增获取客户端ip的方法

---
+++ util/ip/ip.go
@@ -0,0 +1,31 @@
+/**
+ * @Author : nopsky
+ * @Email : cnnopsky@gmail.com
+ * @Date : 2021/10/25 13:34
+ */
+package ip
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+func ClientIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+
+	return ""
+}
