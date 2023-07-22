6bb91c0a816060db51fe775d6365820fff7198b4
nopsky <cnnopsky@gmail.com>
2023-07-22 12:46:11 +0800

    feat: suppoert grpc 1.56.2

--- transport/grpc/resolver/direct/builder.go
+++ transport/grpc/resolver/direct/builder.go
@@ -14,14 +14,15 @@ type directBuilder struct{}
 
 // NewBuilder creates a directBuilder which is used to factory direct resolvers.
 // example:
-//   direct://<authority>/127.0.0.1:9000,127.0.0.2:9000
+//
+//	direct://<authority>/127.0.0.1:9000,127.0.0.2:9000
 func NewBuilder() resolver.Builder {
 	return &directBuilder{}
 }
 
 func (d *directBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	var addrs []resolver.Address
-	for _, addr := range strings.Split(target.Endpoint, ",") {
+	for _, addr := range strings.Split(target.Endpoint(), ",") {
 		addrs = append(addrs, resolver.Address{Addr: addr})
 	}
 	cc.UpdateState(resolver.State{
